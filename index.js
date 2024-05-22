6e56fab29bd90046e062b433a8e2a01d286a3eed
bbayramm <babakulyyewbayram8@gmail.com>
2024-05-22 09:51:27 +0500

    sms

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const bdParser = require("body-parser");
+const cors = require("cors");
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(bdParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.post("/send", (req, res, next) => {
+  try {
+    const io = require("./socket");
+
+    io.getIO().emit("sendOTP", req.body);
+    return res.status(200).json({ mes: "success" });
+  } catch (err) {
+    throw err;
+  }
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Worked on PORT ${PORT}`);
+});
+
+const io = require("./socket").init(server);
+io.on("connection", (socket) => {
+  console.log("IO connected ++++++++++++");
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
